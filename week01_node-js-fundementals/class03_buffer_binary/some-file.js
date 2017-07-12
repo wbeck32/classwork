a36a61d16d426bf84b780d11bdff83075c4a3c38
martypdx <marty@martypdx.com>
2017-07-12 12:05:00 -0700

    commit class work

--- week01_node-js-fundementals/class03_buffer_binary/some-file.js
+++ week01_node-js-fundementals/class03_buffer_binary/some-file.js
@@ -1,10 +1,18 @@
-const assert = require('assert');
-
-const array = [{ name: 'foo' }, 2, 3];
-
-
-assert.deepEqual(array, [{ name: 'foo' }, 2, 3]);
-assert.equal(array, [{ name: 'foo' }, 2, 3]);
+// const assert = require('assert');
+// const array = [{ name: 'foo' }, 2, 3];
+// assert.deepEqual(array, [{ name: 'foo' }, 2, 3]);
+// assert.equal(array, [{ name: 'foo' }, 2, 3]);
+// assert.deepEqual(Object.keys(obj1), Object.keys(obj2));
 
 
-assert.deepEqual(Object.keys(obj1), Object.keys(obj2));
+
+const fs = require('fs');
+// const buffer = fs.readFileSync('./Endianness.pdf');
+// console.log(buffer);
+const obj = { name: 'pdx' };
+// fs.writeFileSync('obj.json', JSON.stringify(obj));
+const buffer = fs.readFileSync('./obj.json');
+buffer[9] = '113';
+fs.writeFileSync('obj.json', buffer);
+// const string = fs.readFileSync('./obj.json', 'utf8');
+// console.log(typeof string);
