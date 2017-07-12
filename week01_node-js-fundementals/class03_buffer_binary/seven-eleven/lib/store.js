46d753011716122f24c5d5495d3a79413169bca7
martypdx <marty@martypdx.com>
2017-07-12 10:07:46 -0700

    some store tests

--- week01_node-js-fundementals/class03_buffer_binary/seven-eleven/lib/store.js
+++ week01_node-js-fundementals/class03_buffer_binary/seven-eleven/lib/store.js
@@ -15,6 +15,17 @@ class Store {
     get(_id) {
         return this.list.find(o => o._id === _id) || null;
     }
+
+    getAll() {
+        return this.list.slice();
+    }
+
+    remove(_id) {
+        const index = this.list.findIndex(o => o._id === _id);
+        if(index < 0) return { removed: false };
+        this.list.splice(index, 1);
+        return { removed: true };
+    }
 }
 
 module.exports = Store;
--- week01_node-js-fundementals/class03_buffer_binary/seven-eleven/tests/store.test.js
+++ week01_node-js-fundementals/class03_buffer_binary/seven-eleven/tests/store.test.js
@@ -3,32 +3,75 @@ const Store = require('../lib/store');
 
 describe('object store', () => {
 
-    let store = null;
-    beforeEach(() => {
-        store = new Store();
-    });
+    describe('CRUD operations', () => {  
+        let store = null;
+        beforeEach(() => {
+            store = new Store();
+        });
 
-    it('saves an gives an id', () => {
         const cat = { type: 'cat', name: 'felix' };
-        const saved = store.save(cat);
-        assert.ok(saved._id);
-        assert.equal(saved.type, cat.type);
-        assert.equal(saved.name, cat.name);
-    });
 
-    it('gets saved object', () => {
-        const cat = { type: 'cat', name: 'felix' };
-        const { _id } = store.save(cat);
-        const got = store.get(_id);
-        assert.equal(got.type, cat.type);
-        assert.equal(got.name, cat.name);
+        function testCat(actual) {
+            assert.equal(actual.type, cat.type);
+            assert.equal(actual.name, cat.name);
+        }
+
+        it('saves an gives an id', () => {
+            const saved = store.save(cat);
+            assert.ok(saved._id);
+            testCat(saved);
+        });
+
+        it('gets saved object', () => {
+            const cat = { type: 'cat', name: 'felix' };
+            const { _id } = store.save(cat);
+            const got = store.get(_id);
+            testCat(got);
+        });
+
+        it('returns remove false if id does not exist', () => {
+            const { removed } = store.remove('bad id');
+            assert.ok(!removed);
+        });
+
+        it('removes a saved object', () => {
+            const { _id } = store.save(cat);
+            const { removed } = store.remove(_id);
+            assert.ok(removed);
+            const got = store.get(_id);
+            assert.equal(got, null);
+        });
     });
 
     describe('get all', () => {
         let store = new Store();
 
+        const cats = [
+            { type: 'cat', name: 'felix' },
+            { type: 'cat', name: 'garfield' },
+            { type: 'cat', name: 'heathcliff' }
+        ];
+
         it('returns empty array when nothing in store', () => {
             assert.deepEqual(store.getAll(), []);
         });
+
+        it('returns clone of list', () => {
+            const list = store.getAll();
+            list.push(4);
+            assert.deepEqual(store.getAll(), []);
+        });
+
+        it('has saved objects', () => {
+            cats.forEach(cat => store.save(cat));
+            assert.deepEqual(store.getAll(), cats);
+        });
+
+        it('removing object means not in getAll', () => {
+            store.remove(cats[1]._id);
+            const got = store.getAll();
+            assert.deepEqual(got, [cats[0], cats[2]]);
+        });
+
     });
 });
