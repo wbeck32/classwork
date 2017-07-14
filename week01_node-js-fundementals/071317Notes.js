07c0c3c7c796ca43308a83b95dddbb225f677fcb
Wendy Beck <webeck@gmail.com>
2017-07-13 22:16:27 -0700

    updates to notes

--- week01_node-js-fundementals/071317Notes.js
+++ week01_node-js-fundementals/071317Notes.js
@@ -32,13 +32,38 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((req, res) => {
-  let body = 'howdy';
-  req.on('data', data => body += data);
-  req.on('end', () => {
-    const user = JSON.parse(parse);
-    console.log(res);
-  });
-  res.end('this');
+// http.createServer((req, res) => {
+//   let body = 'howdy';
+//   req.on('data', data => body += data);
+//   req.on('end', () => {
+//     const user = JSON.parse(parse);
+//     console.log(res);
+//   });
+//   res.end('this');
+// }).listen(3001);
 
+console.log('starting');
+setTimeout(() => {
+  // console.log(err, data);
+  console.log('timeout done');
+}, 5000);
+
+http.createServer((req, res) => {
+  console.log('got a web request');
+  res.end('howdy');
 }).listen(3001);
+
+// closure means that filename can be remembered
+// 'quad core' means that the cpu can only do 4 requests at a time
+// each node server can do one request at a time - run multiple node servers
+function readFile(filename, cb) {
+  // parameters (err, data) is the callback
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if(err) return console.log('ERROR: ', err);
+    console.log('fn: ', filename, 'preview: ', data.slice(20));
+    console.log('file read done');
+  });
+}
+readFile('071317Notes.txt');
+
+console.log('at end of file');
