6669b84fca06c5ada5868a3c4240665fbbf96dbf
martypdx <marty@martypdx.com>
2017-07-17 12:11:28 -0700

    http server and test example

--- week02_node-servers/class06_HTTP-Rest/server.js
+++ week02_node-servers/class06_HTTP-Rest/server.js
@@ -1,37 +1,10 @@
 const http = require('http');
-const url = require('url')
+const app = require('./lib/app');
 
-const server = http.createServer((req, res) => {
-
-    res.setHeader('Content-Type', 'application/json');
-    const parsedUrl = url.parse(req.url, {
-        parseQueryString: true
-    });
-    res.end(JSON.stringify(parsedUrl));
-
-
-
-    // res.setHeader('Content-Type', 'text/html');
-    // if(req.url === '/error') {
-    //     res.statusCode = 400;
-    //     res.statusMessage = 'Oh No!';
-    //     res.end('Warning!');
-    // }
-    // else {
-    //     res.statusCode = 200;
-    //     res.statusMessage = 'Okie Dokie';
-    //     res.write('<h1>very simple http server</h1>');
-    //     if(req.method === 'GET') {
-    //         res.write(`<p>you wanted to get ${req.url}</p>`);
-    //     }
-    //     else {
-    //         res.write('<p>hello world</p>');
-    //     }
-    //     res.end('<p>have a nice day!</p>');
-    // }
-});
+const server = http.createServer(app);
 
 const port = 3000;
 server.listen(port, () => {
+    // eslint-disable-next-line
     console.log('http server running on', server.address());
 });
