352c08244f36428a9387be521471d907a8c9944e
martypdx <marty@martypdx.com>
2017-07-24 15:34:35 -0700

    round out http methods, querying, express static

--- week03_express-mongo/class11_express-basics/basic-express/lib/app.js
+++ week03_express-mongo/class11_express-basics/basic-express/lib/app.js
@@ -1,18 +1,18 @@
-// const express = require('express');
-// const app = express();
-const app = require('express')();
+const express = require('express');
+const app = express();
 const connect = require('./db');
 const bodyParser = require('body-parser');
 const ObjectID = require('mongodb').ObjectID
+const path = require('path');
 
 // a bit of middleware magic for today.
 // create a json body parser and pass to 
 // "app.use"    
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('<p>Hello from Express!</p>');
-});
+// try any request as a "file" in ./public
+const publicDir = path.resolve(__dirname, '../public');
+app.use(express.static(publicDir));
 
 app.post('/tourists', (req, res) => {
     const Tourists = connect.db.collection('tourists');
@@ -24,7 +24,12 @@ app.post('/tourists', (req, res) => {
 
 app.get('/tourists', (req, res) => {
     const Tourists = connect.db.collection('tourists');
-    Tourists.find().toArray()
+    
+    const query = {}
+    if(req.query.from) query.from = req.query.from;
+    if(req.query.name) query.name = req.query.name;
+
+    Tourists.find(query).toArray()
         .then(tourists => res.send(tourists))
         .catch(console.log)
 });
@@ -32,13 +37,15 @@ app.get('/tourists', (req, res) => {
 app.get('/tourists/:id', (req, res) => {
     const Tourists = connect.db.collection('tourists');
     Tourists.findOne({ _id: new ObjectID(req.params.id)})
-        // .then(res => res.ops[0])
         .then(tourist => res.send(tourist))
-        .catch(console.log)
+        .catch(console.log);
 });
 
-app.get('/items', (req, res) => {
-    res.json([{ name: 'FOO' }, { name: 'BAR' }]);
+app.delete('/tourists/:id', (req, res) => {
+    const Tourists = connect.db.collection('tourists');
+    Tourists.removeOne({ _id: new ObjectID(req.params.id)})
+        .then(({ result }) => res.send({ removed: result.n === 1 }))
+        .catch(console.log);    
 });
 
 module.exports = app;
