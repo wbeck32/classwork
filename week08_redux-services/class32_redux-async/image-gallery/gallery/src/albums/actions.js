df2f4315f6940ce5f4b4a1c02d1b99235a286d07
martypdx <marty@martypdx.com>
2017-08-31 13:48:23 -0700

    make async action creators testable

--- week08_redux-services/class32_redux-async/image-gallery/gallery/src/albums/actions.js
+++ week08_redux-services/class32_redux-async/image-gallery/gallery/src/albums/actions.js
@@ -1,40 +1,39 @@
 import * as actions from './constants';
 import api from '../api/albumsApi';
 
-// action creator called "getAlbums"
-export function getAlbums() {
-
-  // it "creates" an action that is a function,
-  // meaning it will get called back with dispatch
-  return dispatch => {
-    dispatch({ type: actions.FETCHING_ALBUMS });
-    
-    api.getAll()
-      .then(
-        albums => {
-          dispatch({ type: actions.FETCHED_ALBUMS, payload: albums });
-        },
-        error => {
-          dispatch({ type: actions.FETCHED_ALBUMS_ERROR, payload: error });
-        }
-      );
-  };
-
-}
-
-export function addAlbum(album) {
-
-  return dispatch => {
-    dispatch({ type: actions.ADDING_ALBUM });
-    
-    api.add(album)
-      .then(
-        saved => {
-          dispatch({ type: actions.ADDED_ALBUM, payload: saved });
-        },
-        err => {
-          dispatch({ type: actions.ADDED_ALBUM_ERROR, payload: err.error });
-        }
-      );
+export function makeGetAlbums(api) {
+  return function() {
+    return function(dispatch) {
+      dispatch({ type: actions.FETCHING_ALBUMS });
+      
+      return api.getAll()
+        .then(
+          albums => {
+            dispatch({ type: actions.FETCHED_ALBUMS, payload: albums });
+          },
+          error => {
+            dispatch({ type: actions.FETCHED_ALBUMS_ERROR, payload: error.error });
+          }
+        );
+    };
   };
 }
+
+export const getAlbums = makeGetAlbums(api);
+
+export const makeAddAlbum = api => album => dispatch => {
+
+  dispatch({ type: actions.ADDING_ALBUM });
+  
+  return api.add(album)
+    .then(
+      saved => {
+        dispatch({ type: actions.ADDED_ALBUM, payload: saved });
+      },
+      err => {
+        dispatch({ type: actions.ADDED_ALBUM_ERROR, payload: err });
+      }
+    );
+};
+
+export const addAlbum = makeAddAlbum(api);
