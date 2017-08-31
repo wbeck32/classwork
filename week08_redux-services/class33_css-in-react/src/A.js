d273e7f09609019ebc90d834365f229979d5b58d
martypdx <marty@martypdx.com>
2017-08-30 18:28:58 -0700

    styled components!

--- week08_redux-services/class33_css-in-react/src/A.js
+++ week08_redux-services/class33_css-in-react/src/A.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import styles from './A.css';
+import styled, { css } from 'styled-components';
 
-export default function A() {
+const color = 'coral';
+
+const ASection = styled.div`
+  p {
+    color: {color}
+  }
+`;
+
+const Button = styled.button`
+  outline: 0;
+  border: 1px solid black;
+  ${props => props.bold ? css`background: steelblue` : css`color: steelblue`};
+`;
+
+function CoolButton(props) {
   return (
-    <div className={styles.label}>
-      <p>Component A</p>
-      <button>Yo!</button>
+    <div className={props.className}>
+      I am a cool button <button>with a button</button>
     </div>
+  );
+}
+
+const StyledCoolButton = styled(CoolButton)`
+  height: 100px;
+  width: 300px;
+  background: lightsteelblue;
+  color: steelblue;
+`;
+
+export default function A({ foo }) {
+  return (
+    <ASection>
+      <p>Component A</p>
+      <Button>Yo!</Button>
+      <Button bold={foo}>Ya!</Button>
+      <StyledCoolButton/>
+    </ASection>
   );
 }
